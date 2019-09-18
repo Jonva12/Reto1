b361299cf631e1256535432a74723ce330efc525
cynda3 <andergonzalez33@gmail.com>
2019-09-18 14:26:18 +0200

    cambios javascript y css

--- Index.php
+++ Index.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="Styles/estilo.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="Scripts/script.js"></script>
 	<title>SimpsonsToday</title>
 </head>
 <body>
@@ -35,26 +37,26 @@
 	</header>
 	<nav>
 		<ul>
-			<li>Descripción</li>
-			<li>Temporadas</li>
-			<li>Reparto</li>
-			<li>Mechandising</li>
-			<li>Vídeos & links</li>
+			<li id="descripcion">Descripción</li>
+			<li id="temporadas">Temporadas</li>
+			<li id="reparto">Reparto</li>
+			<li id="merch">Mechandising</li>
+			<li id="videos">Vídeos & links</li>
 		</ul>
 	</nav>
 
 	<section id="Contenido">
-		<div id="descripcion">
+		<div id="descripcionCont">
 			<p>Los Simpson es una serie estadounidense de animación y comedia, creada por Matt Groening para el canal de televisión de la compañía Fox. La seríe cuenta la vida de una familia tipica trabajadora estadounidense, la cual constituyen los personajes <strong>Homer, Marge, Bart, Lisa y Maggie</strong>*. Esta situada en el pueblo ficticio de Springfield y parodia la cultura americana, su sociedad, televisión y la condición humana.</p>
 			<p>La familia fue concebida por Groening poco antes de solicitar una serie de cortos animados con el productor James L. Brooks. Groening creó una familia disfuncional y nombró a los personajes en honor a los miembros de su propia familia, sustituyendo a Bart por su propio nombre. Los cortometrajes se convirtieron en parte de The Tracey Ullman Show el 19 de abril de 1987. Después de tres temporadas, el boceto se convirtió en un show de media hora en horario estelar y se convirtió en la primera serie de Fox en llegar a las 30 clasificaciones principales en una temporada (1989– 90)</p>
 			<p>Desde su debut el 17 de diciembre de 1989, se han transmitido 662 episodios de Los Simpson. Es la comedia de situación estadounidense de más larga duración, y la serie de televisión estadounidense en horario estelar de mayor duración tanto en temporadas como en número de episodios. The Simpsons Movie, un largometraje, se estrenó en los cines de todo el mundo el 27 de julio de 2007 y recaudó más de $ 527 millones. Luego, el 30 de octubre de 2007, se lanzó un videojuego. Actualmente, The Simpsons terminó de emitirse su trigésima temporada, que comenzó a emitirse el 30 de septiembre de 2018. The Simpsons se renovó por una trigésima primera y trigésima segunda temporada el 6 de febrero de 2019, en la que esta última contendrá el episodio 700. The Simpsons es una producción conjunta de Gracie Films y 20th Century Fox Television y sindicada por 20th Television.</p>
 			<p>Los Simpson recibieron elogios durante sus primeras nueve o diez temporadas, que generalmente se consideran su "Edad de Oro". Time la nombró la mejor serie de televisión del siglo XX, y Erik Adams de The A.V. Club lo llamó "el mayor logro de la televisión independientemente del formato". El 14 de enero de 2000, la familia Simpson recibió una estrella en el Paseo de la Fama de Hollywood. Ha ganado docenas de premios desde que debutó como una serie, incluidos 31 Premios Primetime Emmy, 30 Premios Annie y un Premio Peabody. Frase exclamativa de Homero "D'oh!" ha sido adoptado en el idioma inglés, mientras que The Simpsons ha influido en muchas otras comedias animadas orientadas a adultos. Sin embargo, también ha sido criticado por una disminución de la calidad percibida a lo largo de los años.</p>
 		</div>
-		<div id="temporadas">
+		<div id="temporadasCont">
 			<h3>La serie de "Los Simpsons" actualmente dispone de 32 temporadas.</h3>
 			<h4>Se espera que el </h4>
 		</div>
-		<div id="reparto">
+		<div id="repartoCont">
 			<h3>Hay una larga serie de personajes en Los Simpson de los cuales los mas importantes serían:</h3>
 			<ul>
 				<li><a href="#homer"> Homer J. Simpson</a></li>
@@ -109,16 +111,16 @@
 				<p>En lugar de hablar, Maggie es conocida por producir un sonido característico de "succión" de su chupete. Este efecto de sonido fue proporcionado originalmente por el creador del programa Matt Groening, para los primeros episodios de The Tracey Ullman Show, y también por Gabor Csupo. El ruido de succión se escucha en todas las apariciones de Maggie hasta la fecha, y generalmente es un archivo de audio de las grabaciones iniciales de Groening o Csupo (de los primeros episodios del programa). Además de su ruido de succión, se sabe que Maggie hace otros ruidos, como chillidos ocasionales y balbuceos. En la mayoría de los casos, estas vocalizaciones son proporcionadas por Nancy Cartwright o Yeardley Smith.</p>
 			</div>
 		</div>
-		<div id="merchandising">
+		<div id="merchCont">
 			
 		</div>
-		<div id="videos">
+		<div id="videosCont">
 			
 		</div>
 
 
 
-
+		<hr>
 	</section>
 
 	<footer>
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,44 @@
-var seccion = document.getElementById("Contenido");
+$(document).ready(function () {
+	var seccion = document.getElementById("Contenido");
+
+	document.getElementById("temporadas").addEventListener("click", function(){
+	  	document.getElementById('temporadasCont').style.display = 'block';
+	  	document.getElementById('descripcionCont').style.display = 'none';
+	  	document.getElementById('repartoCont').style.display = 'none';
+	  	document.getElementById('merchCont').style.display = 'none';
+	  	document.getElementById('videosCont').style.display = 'none';
+	});
+
+	document.getElementById("descripcion").addEventListener("click", function(){
+	  	document.getElementById('temporadasCont').style.display = 'none';
+	  	document.getElementById('descripcionCont').style.display = 'block';
+	  	document.getElementById('repartoCont').style.display = 'none';
+	  	document.getElementById('merchCont').style.display = 'none';
+	  	document.getElementById('videosCont').style.display = 'none';
+	});
+
+	document.getElementById("reparto").addEventListener("click", function(){
+	  	document.getElementById('temporadasCont').style.display = 'none';
+	  	document.getElementById('descripcionCont').style.display = 'none';
+	  	document.getElementById('repartoCont').style.display = 'block';
+	  	document.getElementById('merchCont').style.display = 'none';
+	  	document.getElementById('videosCont').style.display = 'none';
+	});
+
+	document.getElementById("merch").addEventListener("click", function(){
+	  	document.getElementById('temporadasCont').style.display = 'none';
+	  	document.getElementById('descripcionCont').style.display = 'none';
+	  	document.getElementById('repartoCont').style.display = 'none';
+	  	document.getElementById('merchCont').style.display = 'block';
+	  	document.getElementById('videosCont').style.display = 'none';
+	});
+
+	document.getElementById("videos").addEventListener("click", function(){
+	  	document.getElementById('temporadasCont').style.display = 'none';
+	  	document.getElementById('descripcionCont').style.display = 'none';
+	  	document.getElementById('repartoCont').style.display = 'none';
+	  	document.getElementById('merchCont').style.display = 'none';
+	  	document.getElementById('videosCont').style.display = 'block';
+	});
+
+});
